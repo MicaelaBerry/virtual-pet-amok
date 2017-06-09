95718c4b9704c0a00519cd950eaf0464c347896d
Micaela <msberry9@gmail.com>
2017-06-09 10:26:30 -0400

    Start of VirtualPet class

---
+++ src/virtual_pets_amok/VirtualPet.java
@@ -0,0 +1,15 @@
+package virtual_pets_amok;
+
+public class VirtualPet {
+
+	private int oilPets;
+	private int feedPets;
+	private int walkPets;
+	private int waterPets;
+	private int healthOfPet;
+	private int happiness;
+	
+	
+	public String getPetType; //get the type of pet it is
+	
+} //end of code
